12a8ef5886c83923549b5754100aeb8132402627
Oussema Dabboussi <oussema.dabboussi99@gmail.com>
2024-12-03 01:16:51 +0100

    test 8

--- src/main.tsx
+++ src/main.tsx
@@ -1,64 +1,70 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { Provider } from "react-redux";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Create from "./Pages/create.tsx";
-import InitiateWidget from "./Pages/initiate-widget.tsx";
-import Widget from "./Pages/widget/index.tsx";
-import Appearance from "./Pages/widget/Pages/appearance.tsx";
-import BrandSettings from "./Pages/widget/Pages/brand-settings.tsx";
-import Details from "./Pages/widget/Pages/details.tsx";
-import store from "./redux/app/store.ts";
-import Publish from "./Pages/widget/Pages/publish.tsx";
+// import Create from "./Pages/create.tsx";
+// import InitiateWidget from "./Pages/initiate-widget.tsx";
+// import Widget from "./Pages/widget/index.tsx";
+// import Appearance from "./Pages/widget/Pages/appearance.tsx";
+// import BrandSettings from "./Pages/widget/Pages/brand-settings.tsx";
+// import Details from "./Pages/widget/Pages/details.tsx";
+// import store from "./redux/app/store.ts";
+// import Trim from "./Pages/trim.tsx";
+// import Publish from "./Pages/widget/Pages/publish.tsx";
 import WidgetEntry from "./widget/Components/Widget.tsx";
-import Trim from "./Pages/trim.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Create />,
-  },
-  {
-    path: "/trim/:id",
-    element: <Trim />,
-  },
-  {
-    path: "/preview/:id",
-    element: <WidgetEntry />,
-  },
-  {
-    path: "/initiate",
-    element: <InitiateWidget />,
-  },
-  {
-    path: "/widget/:id",
-    element: <Widget />,
-    children: [
-      { path: "appearance", element: <Appearance /> },
-      { path: "brand-settings", element: <BrandSettings /> },
-      { path: "details", element: <Details /> },
-      { path: "publish", element: <Publish /> },
-    ],
-  },
-]);
-const root = document.getElementById("root");
-const widget = document.getElementById("widget");
-if (root) {
-  console.log("entered 111");
-  createRoot(root).render(
-    <StrictMode>
-      <Provider store={store}>
-        <RouterProvider router={router} />
-      </Provider>
-    </StrictMode>
-  );
-} else if (!root && widget) {
-  console.log("entered 222");
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Create />,
+//   },
+//   {
+//     path: "/trim/:id",
+//     element: <Trim />,
+//   },
+//   {
+//     path: "/preview/:id",
+//     element: <WidgetEntry />,
+//   },
+//   {
+//     path: "/initiate",
+//     element: <InitiateWidget />,
+//   },
+//   {
+//     path: "/widget/:id",
+//     element: <Widget />,
+//     children: [
+//       { path: "appearance", element: <Appearance /> },
+//       { path: "brand-settings", element: <BrandSettings /> },
+//       { path: "details", element: <Details /> },
+//       { path: "publish", element: <Publish /> },
+//     ],
+//   },
+// ]);
+// const root = document.getElementById("root");
+// const widget = document.getElementById("widget");
+// if (root) {
+//   console.log("entered 111");
+//   createRoot(root).render(
+//     <StrictMode>
+//       <Provider store={store}>
+//         <RouterProvider router={router} />
+//       </Provider>
+//     </StrictMode>
+//   );
+// } else if (!root && widget) {
+//   console.log("entered 222");
 
-  createRoot(widget).render(
-    <StrictMode>
-      <WidgetEntry />
-    </StrictMode>
-  );
-}
+//   createRoot(widget).render(
+//     <StrictMode>
+//       <WidgetEntry />
+//     </StrictMode>
+//   );
+// }
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <WidgetEntry />
+  </StrictMode>
+);
